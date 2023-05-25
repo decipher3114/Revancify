7b0dcdeafcb52c0d4ce23a48addd208538156c7c
decipher3114 <shauryasiwach26@gmail.com>
2023-05-25 21:52:43 +0530

    fix: file copy error due to ":" in versionName

--- main.sh
+++ main.sh
@@ -334,7 +334,7 @@ checkResources() {
 getAppVer() {
     if [ "$rootStatus" == "root" ] && su -c "pm list packages | grep -q $pkgName" && [ "$allowVersionDowngrade" == "false" ]; then
         selectedVer=$(su -c dumpsys package "$pkgName" | sed -n '/versionName/s/.*=//p' | sed -n '1p')
-        appVer="${selectedVer// /.}"
+        appVer="$(sed 's/ /./g;s/-/./g;s/\://g' <<< "$selectedVer")"
     fi
     if [ "${#appVerList[@]}" -lt 2 ]; then
         internet || return 1
@@ -353,7 +353,7 @@ versionSelector() {
     if [ "$selectedVer" == "Auto Select" ]; then
         selectedVer=$(jq -n -r --argjson includedPatches "$includedPatches" --arg pkgName "$pkgName" '$includedPatches[] | select(.pkgName == $pkgName) | .versions[-1]')
     fi
-    appVer="${selectedVer// /.}"
+    appVer="$(sed 's/ /./g;s/-/./g;s/\://g' <<< "$selectedVer")"
 }
 
 checkPatched() {
@@ -440,7 +440,7 @@ fetchCustomApk() {
     fileAppName=$(grep "application-label:" <<<"$aaptData" | sed -e 's/application-label://' -e 's/'\''//g')
     appName="$(sed 's/\./-/g;s/ /-/g' <<<"$fileAppName")"
     selectedVer=$(grep "package:" <<<"$aaptData" | sed -e 's/.*versionName='\''//' -e 's/'\'' platformBuildVersionName.*//')
-    appVer="${selectedVer// /.}"
+    appVer="$(sed 's/ /./g;s/-/./g;s/\://g' <<< "$selectedVer")"
     if [ "$rootStatus" == "root" ] && su -c "pm list packages | grep -q $pkgName" && [ "$allowVersionDowngrade" == "false" ]; then
         installedVer=$(su -c dumpsys package "$pkgName" | sed -n '/versionName/s/.*=//p' | sed -n '1p')
         if [ "$installedVer" != "$selectedVer" ]; then
