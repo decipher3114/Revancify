35fe492a7af37a4b1b9fdc6d397ca3962db16cf7
decipher3114 <shauryasiwach26@gmail.com>
2024-11-11 19:52:03 +0530

    fix: argument specifier

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 [ -z "$TERMUX_VERSION" ] && echo -e "Termux not detected !!" && exit 1
 BIN="$PREFIX/bin/revancify"
-curl -sL "https://github.com/decipher3114/Revancify/raw/refs/heads/bump/v2.0/revancify" -O "$BIN"
+curl -sL "https://github.com/decipher3114/Revancify/raw/refs/heads/bump/v2.0/revancify" -o "$BIN"
 [ -e "$BIN" ] && chmod +x "$BIN" && "$BIN"
