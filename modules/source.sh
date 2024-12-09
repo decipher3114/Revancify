450ba0a69053d59d8cbf90183a6ca922ca0c683f
decipher3114 <shauryasiwach26@gmail.com>
2024-12-09 18:44:10 +0530

    fix: don't cache sources menu

--- modules/source.sh
+++ modules/source.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 changeSource() {
-    local SELECTED_SOURCE
-    [ -n "$SOURCES" ] || readarray -t SOURCES < <(jq -r --arg SOURCE "$SOURCE" '.[] | .source | ., if . == $SOURCE then "on" else "off" end' "$SRC"/sources.json)
+    local SELECTED_SOURCE SOURCES_INFO
+    readarray -t SOURCES_INFO < <(jq -r --arg SOURCE "$SOURCE" '.[] | .source | ., if . == $SOURCE then "on" else "off" end' "$SRC"/sources.json)
     SELECTED_SOURCE=$("${DIALOG[@]}" \
         --title '| Source Selection Menu |' \
         --no-cancel \
         --no-items \
         --ok-label 'Done' \
         --radiolist "$NAVIGATION_HINT\n$SELECTION_HINT" -1 -1 0 \
-        "${SOURCES[@]}" 2>&1 > /dev/tty
+        "${SOURCES_INFO[@]}" 2>&1 > /dev/tty
     )
 
     [ "$SOURCE" == "$SELECTED_SOURCE" ] && return
