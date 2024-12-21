238850441e47d8fd6534dfd8a20398f7c3c3eb45
decipher3114 <shauryasiwach26@gmail.com>
2024-12-21 20:44:13 +0530

    fix: use correct file name

--- root/umount.sh
+++ root/umount.sh
@@ -6,6 +6,6 @@ am force-stop "$PKG_NAME"
 grep "$PKG_NAME" /proc/mounts | cut -d " " -f 2 | sed "s/apk.*/apk/" | xargs -r umount -l
 am force-stop "$PKG_NAME"
 pm clear --cache-only "$PKG_NAME"
-rm "/data/adb/service.d/mount_revanced_$PKG_NAME.sh"
-rm "/data/adb/post-fs-data.d/umount_revanced_$PKG_NAME.sh"
+rm -f "/data/adb/service.d/mount_$PKG_NAME.sh"
+rm -f "/data/adb/post-fs-data.d/umount_$PKG_NAME.sh"
 rm -f "/data/local/tmp/revancify/$PKG_NAME.apk"
