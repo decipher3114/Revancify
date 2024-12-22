8be9e9f51191d37c9427a571db2950fa8c2d4ca5
decipher3114 <shauryasiwach26@gmail.com>
2024-12-22 19:00:06 +0530

    refactor: check parse status before writing to file

--- modules/json/api.sh
+++ modules/json/api.sh
@@ -5,89 +5,89 @@ parseJsonFromAPI() {
     
     notify info "Please Wait!!\nParsing JSON file for $SOURCE patches from API."
 
-    if ! RESPONSE=$("${CURL[@]}" "$JSON_URL" | jq -c '.' 2> /dev/null); then
-        unset JSON_URL
-        return 1
-    fi
-
-    AVAILABLE_PATCHES=$(jq -c '
-        reduce .[] as {
-            name: $PATCH,
-            use: $USE,
-            compatiblePackages: $COMPATIBLE_PKGS,
-            options: $OPTIONS
-        } (
-            [];
-            (
-                $OPTIONS |
-                if length != 0 then
-                    map(
-                        . |= {"patchName": $PATCH} + . |
-                        .type |= (
-                            if test("List") then
-                                "StringArray"
-                            elif test("Boolean") then
-                                "Boolean"
-                            elif test("Long|Int|Float") then
-                                "Number"
-                            else
-                                "String"
-                            end
-                        ) |
-                        .values |= (
-                            if . != null then
-                                [to_entries[] | (.value | tostring) + " (" + .key + ")"]
-                            else
-                                []
-                            end
+    if ! AVAILABLE_PATCHES=$("${CURL[@]}" "$JSON_URL" | jq -c '
+            reduce .[] as {
+                name: $PATCH,
+                use: $USE,
+                compatiblePackages: $COMPATIBLE_PKGS,
+                options: $OPTIONS
+            } (
+                [];
+                (
+                    $OPTIONS |
+                    if length != 0 then
+                        map(
+                            . |= {"patchName": $PATCH} + . |
+                            .type |= (
+                                if test("List") then
+                                    "StringArray"
+                                elif test("Boolean") then
+                                    "Boolean"
+                                elif test("Long|Int|Float") then
+                                    "Number"
+                                else
+                                    "String"
+                                end
+                            ) |
+                            .values |= (
+                                if . != null then
+                                    [to_entries[] | (.value | tostring) + " (" + .key + ")"]
+                                else
+                                    []
+                                end
+                            )
                         )
-                    )
-                else
-                    .
-                end
-            ) as $OPTIONS |
-            [
-                $COMPATIBLE_PKGS |
-                if . == null then
-                    {"name": null, "versions":[]}
-                else
-                    to_entries[] |
-                    {"name": .key, "versions": (.value // [])}
-                end
-            ] as $COMPATIBLE_PKGS |
-            reduce $COMPATIBLE_PKGS[] as {name: $PKG_NAME, versions: $VERSIONS} (
-                .;
-                if any(.[]; .pkgName == $PKG_NAME) then
-                    .
-                else
-                    . |= .[0:-1] + [
-                        {
-                            "pkgName": $PKG_NAME,
-                            "versions": [],
-                            "patches": {
-                                "recommended": [],
-                                "optional": []
-                            },
-                            "options": []
-                        }
-                    ] + .[-1:]
-                end |
-                map(
-                    if .pkgName == $PKG_NAME then
-                        .versions |= (. += $VERSIONS | unique | sort) |
-                        .patches |= (
-                            if $USE then
-                                .recommended += [$PATCH]
-                            else
-                                .optional += [$PATCH]
-                            end
-                        ) |
-                        .options += $OPTIONS
                     else
                         .
                     end
+                ) as $OPTIONS |
+                [
+                    $COMPATIBLE_PKGS |
+                    if . == null then
+                        {"name": null, "versions":[]}
+                    else
+                        to_entries[] |
+                        {"name": .key, "versions": (.value // [])}
+                    end
+                ] as $COMPATIBLE_PKGS |
+                reduce $COMPATIBLE_PKGS[] as {name: $PKG_NAME, versions: $VERSIONS} (
+                    .;
+                    if any(.[]; .pkgName == $PKG_NAME) then
+                        .
+                    else
+                        . |= .[0:-1] + [
+                            {
+                                "pkgName": $PKG_NAME,
+                                "versions": [],
+                                "patches": {
+                                    "recommended": [],
+                                    "optional": []
+                                },
+                                "options": []
+                            }
+                        ] + .[-1:]
+                    end |
+                    map(
+                        if .pkgName == $PKG_NAME then
+                            .versions |= (. += $VERSIONS | unique | sort) |
+                            .patches |= (
+                                if $USE then
+                                    .recommended += [$PATCH]
+                                else
+                                    .optional += [$PATCH]
+                                end
+                            ) |
+                            .options += $OPTIONS
+                        else
+                            .
+                        end
+                    )
                 )
-            )
-        )' <<< "$RESPONSE" > "assets/$SOURCE/Patches-$PATCHES_VERSION.json" 2> /dev/null
-    )
+            )' \
+    2> /dev/null); then
+        unset JSON_URL AVAILABLE_PATCHES
+        return 1
+    fi
+
+    echo "$AVAILABLE_PATCHES" > "assets/$SOURCE/Patches-$PATCHES_VERSION.json"
 }
