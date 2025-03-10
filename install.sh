3363e7d445fd97d59ea9fa38ecbf2d0dbc65c36b
Xisrr1 <133952156+Xisrr1@users.noreply.github.com>
2025-03-10 13:28:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 [ -z "$TERMUX_VERSION" ] && echo -e "Termux not detected !!" && exit 1
 BIN="$PREFIX/bin/revancify"
-curl -sL "https://github.com/Xisrr1/Revancify-Xisr/raw/refs/heads/main/revancify" -o "$BIN"
+curl -sL "https://github.com/Xisrr1/Revancify/raw/refs/heads/main/revancify" -o "$BIN"
 [ -e "$BIN" ] && chmod +x "$BIN" && "$BIN"
