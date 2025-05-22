c03060b41872a51e5452d0a2d9db8ea6dc9523ed
decipher3114 <shauryasiwach26@gmail.com>
2025-05-23 01:18:03 +0530

    feat: fetch versions lit for 5 pages in parallel

--- modules/app/version.sh
+++ modules/app/version.sh
@@ -1,76 +1,94 @@
 #!/usr/bin/bash
 
 scrapeVersionsList() {
-    local PAGE VERSIONS
-    PAGE=$("${CURL[@]}" -A "$USER_AGENT" "https://www.apkmirror.com/uploads/?appcategory=$APKMIRROR_APP_NAME" 2>&1)
+    local PAGE_CONTENTS PAGE_JSON MERGED_JSON
+    local IDX MAX_PAGE_COUNT
 
-    if [ "$PAGE" == "" ]; then
+    MAX_PAGE_COUNT=5
+
+    for ((IDX = 1; IDX <= MAX_PAGE_COUNT; IDX++)); do
+        TMP_FILES[IDX]=$(mktemp)
+        "${CURL[@]}" -A "$USER_AGENT" "https://www.apkmirror.com/uploads/page/$IDX/?appcategory=$APKMIRROR_APP_NAME" > "${TMP_FILES[$IDX]}" 2> /dev/null &
+    done
+    wait
+
+    for ((IDX = 1; IDX <= MAX_PAGE_COUNT; IDX++)); do
+        PAGE_CONTENTS[IDX]=$(cat "${TMP_FILES[$IDX]}")
+        rm -f "${TMP_FILES[$IDX]}"
+    done
+
+    for ((IDX = 1; IDX <= MAX_PAGE_COUNT; IDX++)); do
+        PAGE_JSON[IDX]=$(
+            pup -c 'div.widget_appmanager_recentpostswidget div.listWidget div:not([class]) json{}' <<< "${PAGE_CONTENTS[$IDX]}" |
+                jq -rc '
+                .[].children as $CHILDREN |
+                {
+                    version: $CHILDREN[1].children[0].children[1].text,
+                    info: $CHILDREN[0].children[0].children[1].children[0].children[0].children[0]
+                } |
+                {
+                    version: .version,
+                    tag: (
+                        .info.text | ascii_downcase |
+                        if test("beta") then
+                            "[BETA]"
+                        elif test("alpha") then
+                            "[ALPHA]"
+                        else
+                            "[STABLE]"
+                        end
+                    ),
+                    url: .info.href
+                }
+            '
+        )
+    done
+
+    MERGED_JSON=$(jq -s '.' <<< "$(printf '%s\n' "${PAGE_JSON[@]}")")
+
+    if [[ "$MERGED_JSON" == "[]" ]]; then
         notify msg "Unable to fetch versions !!\nThere is some problem with your internet connection. Disable VPN or Change your network."
         TASK="CHOOSE_APP"
         return 1
     fi
 
     readarray -t VERSIONS_LIST < <(
-        pup -c 'div.widget_appmanager_recentpostswidget div.listWidget div:not([class]) json{}' <<< "$PAGE" |
-            jq -rc \
-                --arg PKG_NAME "$PKG_NAME" \
-                --arg INSTALLED_VERSION "$INSTALLED_VERSION" \
-                --arg ALLOW_APP_VERSION_DOWNGRADE "$ALLOW_APP_VERSION_DOWNGRADE" \
-                --argjson AVAILABLE_PATCHES "$AVAILABLE_PATCHES" '
-                (
-                    $AVAILABLE_PATCHES[] |
-                    select(.pkgName == $PKG_NAME) |
-                    .versions
-                ) as $SUPPORTED_VERSIONS |
-                [
-                    .[].children |
-                    .[1].children.[0].children.[1].text as $VERSION |
-                    .[0].children.[0].children.[1].children.[0].children.[0].children.[0] as $INFO |
-                    {
-                        "version": ($VERSION),
-                        "tag": (
-                            if ($SUPPORTED_VERSIONS | index($VERSION)) != null then
-                                "[RECOMMENDED]"
-                            else
-                                (
-                                    $INFO.text |
-                                    if test("beta"; "i") then
-                                        "[BETA]"
-                                    elif test("alpha"; "i") then
-                                        "[ALPHA]"
-                                    else
-                                        "[STABLE]"
-                                    end
-                                )
-                            end
-                        ),
-                        "url": $INFO.href
-                    }
-                ] |
-                if $INSTALLED_VERSION != "" then
-                    if ($ALLOW_APP_VERSION_DOWNGRADE | test("off")) then
-                        .[0:(map(.version == $INSTALLED_VERSION) | index(true) + 1)]
-                    end |
-                    map(
-                        if .version == $INSTALLED_VERSION then
-                            .tag |= "[INSTALLED]"
-                        end
-                    )
-                end |
-                (
-                    if any(.[]; .tag == "[RECOMMENDED]") then
-                        (first(.[] | select(.tag == "[RECOMMENDED]"))), "Auto Select|[RECOMMENDED]"
-                    elif $INSTALLED_VERSION != "" then
-                        .[-1], "Auto Select|[INSTALLED]"
-                    else
-                        empty
-                    end,
-                    (
-                        .[] |
-                        ., "\(.version)|\(.tag)"
-                    )
-                )
-            '
+        jq -rc \
+            --arg PKG_NAME "$PKG_NAME" \
+            --arg INSTALLED_VERSION "$INSTALLED_VERSION" \
+            --arg ALLOW_APP_VERSION_DOWNGRADE "$ALLOW_APP_VERSION_DOWNGRADE" \
+            --argjson AVAILABLE_PATCHES "$AVAILABLE_PATCHES" '
+            . as $ALL_VERSIONS |
+            (
+                $AVAILABLE_PATCHES[] |
+                select(.pkgName == $PKG_NAME) |
+                .versions
+            ) as $SUPPORTED_VERSIONS |
+            $ALL_VERSIONS |
+            map(
+                .version as $VERSION |
+                if ($SUPPORTED_VERSIONS | index($VERSION)) != null then
+                    .tag = "[RECOMMENDED]"
+                elif .version == $INSTALLED_VERSION then
+                    .tag = "[INSTALLED]"
+                else
+                    .
+                end
+            ) |
+            (
+                if any(.[]; .tag == "[RECOMMENDED]") then
+                    (first(.[] | select(.tag == "[RECOMMENDED]"))), "Auto Select|[RECOMMENDED]"
+                elif $INSTALLED_VERSION != "" then
+                    .[-1], "Auto Select|[INSTALLED]"
+                else
+                    empty
+                end
+            ),
+            (
+                .[] |
+                ., "\(.version)|\(.tag)"
+            )
+        ' <<< "$MERGED_JSON"
     )
 }
 
