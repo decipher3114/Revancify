1d99c03617b04bdefd0d90f94c37e42b4ed1c4b7
decipher <84389932+decipher3114@users.noreply.github.com>
2025-06-05 18:02:48 +0530

    Revert "feat: Add Rish-based installer with signature verification and downgr…" (#162)
    This reverts commit 3b7c563942d9ecf1350e950c9bdf7f90b9262366.

--- modules/system/install.sh
+++ modules/system/install.sh
@@ -4,8 +4,6 @@ installApp() {
     local CANONICAL_VER
     if [ "$ROOT_ACCESS" == true ]; then
         mountApp
-    elif [ "$RISH_ACCESS" == true ]; then
-        installAppRish
     else
         notify info "Copying patched $APP_NAME apk to Internal Storage..."
         CANONICAL_VER=${APP_VER//:/}
